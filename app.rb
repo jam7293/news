8b7bd23e58e61959283ed96889993d2b3bcf6b2e
jam7293 <julio.morales@kellogg.northwestern.edu>
2020-02-23 21:19:45 +0000

    updates

--- app.rb
+++ app.rb
@@ -16,19 +16,16 @@ forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 # pp = pretty print
 # use instead of `puts` to make reading a hash a lot easier
 # e.g. `pp forecast`
-# pp forecast
-
 current_temperature = forecast["currently"]["temperature"]
 conditions = forecast["currently"]["summary"]
 puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
-puts forecast["daily"]["data"]
-high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
-puts high_temperature
-puts forecast["daily"]["data"][1]["temperatureHigh"]
-puts forecast["daily"]["data"][2]["temperatureHigh"]
+# high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+# puts high_temperature
+# puts forecast["daily"]["data"][1]["temperatureHigh"]
+# puts forecast["daily"]["data"][2]["temperatureHigh"]
 
 for day in forecast["daily"]["data"]
-    puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
+  puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
 end
 
 get "/" do
@@ -42,14 +39,11 @@ get "/map" do
 end
 
 get "/news" do
-    view "ask"
-end
-
-get "/news" do
-      results = Geocoder.search(params["q"])
-      lat_long = results.first.coordinates
-      "#{lat_long[0]}" "#{lat_long[1]}"
-       url = "https://newsapi.org/v2/top-headlines?country=us&apikey=3a20f00aca104a0683a7d2a229e8f8fe"
-       news = HTTparty.get(url).parsed_response.to_hash
-       pp news
+  results = Geocoder.search(params["q"])
+  lat_long = results.first.coordinates
+  "#{lat_long[0]}" "#{lat_long[1]}"
+  url = "https://newsapi.org/v2/top-headlines?country=us&apikey=3a20f00aca104a0683a7d2a229e8f8fe"
+  news = HTTparty.get(url).parsed_response.to_hash
+  pp news
+  view "news"
 end
